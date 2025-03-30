3a23ca49124f7a1a4196682011a46b4191174c34
cx2017 <3036155452@qq.com>
2025-03-30 20:59:57 +0800

    [fix] 测试代码

--- src/StrPool.test.ts
+++ src/StrPool.test.ts
@@ -17,14 +17,16 @@ console.log(d.toString())
 let d1 = StrPool.join(s, s2, s3)
 console.log(d1)
 console.log(d1.toString())
-let s0 = "123456789".repeat(1010000);
+let s0 = "123456789".repeat(0.5e7);
 let bigpool = new StrPool({ str: s0 })
 let s12 = bigpool.slice(10000, 10009);
-let s23 = bigpool.slice(10010, 10001);
+let s23 = bigpool.slice(10010, 10014);
 let s33 = StrPool.join(s12, s23);
 // bigpool.del()
 process.stdin.read();
 console.log(process.memoryUsage());
+console.log(s33.toString())
 bigpool.del();
+bigpool = undefined!;
 console.log(process.memoryUsage());
 console.log(s33.toString())
